fd65b956ce44afe0d8aa3a16f0779239703b2319
Karen-Hoil <karenhoil2004@gmail.com>
2023-06-30 12:33:49 -0500

    se hizo el actualizar y eliminar

--- controllers/categoriasControllers.js
+++ controllers/categoriasControllers.js
@@ -13,6 +13,21 @@ const obtenerCategorias = (req,res) => {
     });
 };
 
+const obtenerCategoriasPorId = (req,res) =>{
+    const id = req.params.id_categoria;
+    
+    connection.query('SELECT*FROM categorias WHERE id_categoria = ?', [id], (error,results) =>{
+        if(error){
+            console.error("Error al eliminar la categoria", error);
+            res.status(500).json({error:"Ocurrio un error al obtener categoria"});
+        }else if (results.length === 0){
+            res.status(500).json({error: "La categoria no fue encontrada"})
+        }else{
+            res.json(results[0]);
+        }
+    });
+}
+
 const crearCategorias =(req, res) => {
     const {nombre} = req.body; //req.body porque lo envias en el cuerpo de la pagina
     connection.query('INSERT INTO categorias (nombre) VALUES (?)', [nombre], (error, results) =>{
@@ -25,8 +40,39 @@ const crearCategorias =(req, res) => {
     });
 }
 
+const eliminarCategoriasPorId = (req,res) =>{
+    const id = req.params.id_categoria;
+
+    connection.query('DELETE FROM categorias WHERE id_categoria = ?', [id], (error,results) =>{
+        if(error){
+            console.error("Error al actualizar la categoria", error);
+            res.status(500).json({error:"Ocurrio un error al obtener categoria"});
+        }else{
+            res.json({message:"La categoria fue eliminada correctamente"});
+        }
+    });
+}
+
+const actualizarCategoriasPorId = (req,res) => {
+    const id = req.params.id_categoria;
+    const {nombre} = req.body;
+
+    connection.query('UPDATE categorias SET nombre = ? WHERE id_categoria = ?', [nombre, id], (error,results) =>{
+        if(error){
+            res.status(500).json({error:"Ocurrio un error al actualizar la categoria"});
+        }else{
+            res.json({message:"La categoria se actualizo correctamente"});
+        }
+    });
+}
+
 module.exports = {
     obtenerCategorias,
+    obtenerCategoriasPorId,
     crearCategorias,
+    actualizarCategoriasPorId,
+    eliminarCategoriasPorId,
 }
+
+
 
--- router/categoriasRouter.js
+++ router/categoriasRouter.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 //rutas para las categorias
 router.get('/', categoriasControllers.obtenerCategorias);
+router.get('/:id_categoria', categoriasControllers.obtenerCategoriasPorId);
 router.post('/', categoriasControllers.crearCategorias);
+router.put('/:id_categoria', categoriasControllers.actualizarCategoriasPorId);
+router.delete('/:id_categoria', categoriasControllers.eliminarCategoriasPorId);
 
 module.exports = router;
