77f7a2b9c96a3737e4e181faa0487ba112d36cff
Karen-Hoil <karenhoil2004@gmail.com>
2023-07-06 14:01:24 -0500

    Se hizo la api de productos

--- controllers/categoriasControllers.js
+++ controllers/categoriasControllers.js
@@ -18,7 +18,7 @@ const obtenerCategoriasPorId = (req,res) =>{
     
     connection.query('SELECT*FROM categorias WHERE id_categoria = ?', [id], (error,results) =>{
         if(error){
-            console.error("Error al eliminar la categoria", error);
+            console.error("Error al obtener la categoria", error);
             res.status(500).json({error:"Ocurrio un error al obtener categoria"});
         }else if (results.length === 0){
             res.status(500).json({error: "La categoria no fue encontrada"})
@@ -29,8 +29,8 @@ const obtenerCategoriasPorId = (req,res) =>{
 }
 
 const crearCategorias =(req, res) => {
-    const {nombre} = req.body; //req.body porque lo envias en el cuerpo de la pagina
-    connection.query('INSERT INTO categorias (nombre) VALUES (?)', [nombre], (error, results) =>{
+    const {nombre, imagen_categoria} = req.body; //req.body porque lo envias en el cuerpo de la pagina
+    connection.query('INSERT INTO categorias (nombre, imagen_categoria) VALUES (?, ?)', [nombre, imagen_categoria], (error, results) =>{
         if(error){
             console.error("Error al agregar categoria", error);
             res.status(500).json({error:"Error al agregar categoria"});
@@ -55,10 +55,11 @@ const eliminarCategoriasPorId = (req,res) =>{
 
 const actualizarCategoriasPorId = (req,res) => {
     const id = req.params.id_categoria;
-    const {nombre} = req.body;
+    const {nombre, imagen_categoria} = req.body;
 
-    connection.query('UPDATE categorias SET nombre = ? WHERE id_categoria = ?', [nombre, id], (error,results) =>{
+    connection.query('UPDATE categorias SET nombre = ?, imagen_categoria = ? WHERE id_categoria = ?', [nombre, imagen_categoria, id], (error,results) =>{
         if(error){
+            console.error("Error al actualizar categoria", error);
             res.status(500).json({error:"Ocurrio un error al actualizar la categoria"});
         }else{
             res.json({message:"La categoria se actualizo correctamente"});
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 
 const categoriasRouter = require('./router/categoriasRouter');
+const productosRouter = require('./router/productosRouter');
+
 
 //app va a tener todos los atributos y metodos de express
 const app = express();
@@ -13,6 +15,8 @@ app.use(express.json());
 
 //Ruta al router
 app.use('/categorias', categoriasRouter);
+app.use('/productos', productosRouter);
+
 
 //  app.get('/',(requ,res) => {
 //     res.send("<h1>Hola pikachu</h1>");
